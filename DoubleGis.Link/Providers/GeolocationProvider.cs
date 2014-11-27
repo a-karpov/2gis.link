4a1c6c4fb8c3cc842b808b91b1c60f188462e178
a.karpov <a.karpov@2gis.ru>
2014-11-27 18:07:09 +0600

    Потерянный файл.

---
+++ DoubleGis.Link/Providers/GeolocationProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net;
+using MelissaData;
+
+namespace DoubleGis.Link.Providers
+{
+	public class GeolocationProvider
+	{
+		public IEnumerable<IPCheckEntity> GetLocation()
+		{
+			var client = new IPCheckContainer(new Uri("https://api.datamarket.azure.com/Data.ashx/MelissaData/IPCheck/v1/"));
+			
+			client.Credentials = new NetworkCredential("accountKey", ConfigurationManager.AppSettings["ipCheckKey"]);
+			return  client.SuggestIPAddresses("216.231.3.166",5,0.7).Execute();
+		}
+	}
+}
