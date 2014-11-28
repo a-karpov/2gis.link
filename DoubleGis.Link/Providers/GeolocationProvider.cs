5f838457f085160b70ddc3b06ba7252603da9882
a.karpov <a.karpov@2gis.ru>
2014-11-28 15:31:08 +0600

    Доработано кэширующее определение локации.

--- DoubleGis.Link/Providers/GeolocationProvider.cs
+++ DoubleGis.Link/Providers/GeolocationProvider.cs
@@ -1,19 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Net;
+using DoubleGis.Link.Models;
 using MelissaData;
 
 namespace DoubleGis.Link.Providers
 {
 	public class GeolocationProvider
 	{
-		public IEnumerable<IPCheckEntity> GetLocation()
+		private readonly IndexStorage _indexStorage;
+
+		public GeolocationProvider(IndexStorage indexStorage)
 		{
+			_indexStorage = indexStorage;
+		}
+
+		public IEnumerable<Geolocation> GetLocation(string ip)
+		{
+			var locations = _indexStorage.FindGeolocation(ip);
+
+			if (locations.Any())
+			{
+				return locations;
+			}
+
 			var client = new IPCheckContainer(new Uri("https://api.datamarket.azure.com/Data.ashx/MelissaData/IPCheck/v1/"));
-			
 			client.Credentials = new NetworkCredential("accountKey", ConfigurationManager.AppSettings["ipCheckKey"]);
-			return  client.SuggestIPAddresses("216.231.3.166",5,0.7).Execute();
+
+			locations =  client.SuggestIPAddresses(ip,5,0.7).Execute()
+				.OrderByDescending(e => e.Confidence)
+				.Select(e => new Geolocation
+				{
+					Country = e.Country,
+					City = e.City,
+					Ip = ip,
+					Lat = e.Latitude,
+					Lon = e.Longitude,
+					Confidence = e.Confidence
+				});
+
+			foreach (var geolocation in locations)
+			{
+				_indexStorage.IndexGeolocation(geolocation);
+			}
+
+			return locations;
 		}
 	}
 }
--- DoubleGis.Link/Providers/IndexStorage.cs
+++ DoubleGis.Link/Providers/IndexStorage.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using DoubleGis.Link.Models;
+using Nest;
 
 namespace DoubleGis.Link.Providers
 {
 	public class IndexStorage
 	{
+		public IEnumerable<Geolocation> FindGeolocation(string ip)
+		{
+			var node = new Uri("http://23.101.8.162:9200/");
+			var settings = new ConnectionSettings(node, "2gislink");
+			var client = new ElasticClient(settings);
+
+			var found = client.Search<Geolocation>(s => s.Size(1).Query(q => q.Term(g => g.Ip, ip)).Sort(sort => sort.OnField(g => g.Confidence).Descending()));
+			return found.Documents;
+		}
+
+		public void IndexGeolocation(Geolocation geolocation)
+		{
+			var node = new Uri("http://23.101.8.162:9200/");
+			var settings = new ConnectionSettings(node, "2gislink");
+			var client = new ElasticClient(settings);
+
+			client.Index(geolocation);
+		}
 	}
 }
