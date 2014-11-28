81d9a91477adc5a97029ff57b8c8e2eb01d095e6
a.karpov <a.karpov@2gis.ru>
2014-11-28 15:31:19 +0600

    Потерянный файл

---
+++ DoubleGis.Link/Models/Geolocation.cs
@@ -0,0 +1,12 @@
+﻿namespace DoubleGis.Link.Models
+{
+	public class Geolocation
+	{
+		public string Ip { get; set; }
+		public string Country { get; set; }
+		public string City { get; set; }
+		public double Lat { get; set; }
+		public double Lon { get; set; }
+		public short Confidence { get; set; }
+	}
+}
