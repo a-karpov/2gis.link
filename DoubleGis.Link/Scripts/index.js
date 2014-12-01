6ceb5aaf09423e359b3a3f79478b6ee3edcb7beb
a.karpov <a.karpov@2gis.ru>
2014-12-01 15:33:19 +0600

    Потерянный файл.

---
+++ DoubleGis.Link/Scripts/index.js
@@ -0,0 +1,15 @@
+﻿(function() {
+    function IndexViewModel() {
+        var self = this;
+
+        self.what = ko.observable();
+
+        self.onSearch = function() {
+            if (self.what()) {
+                location = self.what();
+            }
+        }
+    }
+
+    ko.applyBindings(new IndexViewModel);
+})();
